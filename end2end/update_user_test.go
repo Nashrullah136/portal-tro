e7a138ca951681c58569749551706ee68186916d
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-06-18 20:43:48 +0700

    add update user test

---
+++ end2end/update_user_test.go
@@ -0,0 +1,54 @@
+package end2end
+
+import (
+	"nashrul-be/crm/end2end/testutil"
+	"testing"
+)
+
+func Test_update_user(t *testing.T) {
+	testData, err := testutil.ReadYamlFile("update_user.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, data := range testData {
+		t.Run(data.Name, func(t *testing.T) {
+			e, err := testutil.InitTest(t)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if req, exist := data.Control["create"]; exist {
+				testutil.CreateUser(e, req)
+			}
+			var auth map[string]string
+			loginAs, ok := data.Control["loginAs"].(string)
+			if ok {
+				switch loginAs {
+				case "user":
+					auth = testutil.LoginAsUser(e)
+				case "admin":
+					auth = testutil.LoginAsAdmin(e)
+				}
+			}
+			responseBody := e.PATCH("/users/" + data.Data["username"].(string)).WithHeaders(auth).
+				WithJSON(data.Data["update"]).Expect().Status(data.Expect["code"].(int)).JSON().Object()
+			responseBody.Value("code").IsNumber().IsEqual(data.Expect["code"])
+			if data.Control["case"].(string) == "success" {
+				responseData := responseBody.Value("data").Object()
+				responseData.Value("name").IsString().IsEqual(data.Expect["name"])
+				responseData.Value("username").IsString().IsEqual(data.Expect["username"])
+				responseData.Value("role").IsString().IsEqual(data.Expect["role"])
+				responseData.Value("created_by").IsString().IsEqual("admin")
+				responseData.Value("updated_by").IsString().IsEqual("admin")
+				testutil.Login(e, data.Expect["login"])
+			}
+			t.Cleanup(func() {
+				req, exist := data.Control["create"]
+				if exist {
+					username := req.(map[string]any)["username"].(string)
+					db, _ := testutil.GetConn()
+					db.Table("users").Where("username = ?", username).Delete(&map[string]any{})
+				}
+			})
+		})
+	}
+}
