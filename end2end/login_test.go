70ddbc57300919eef945dcf8a47ed21e77308d43
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-06-18 20:43:36 +0700

    add login test

---
+++ end2end/login_test.go
@@ -0,0 +1,28 @@
+package end2end
+
+import (
+	"nashrul-be/crm/end2end/testutil"
+	"testing"
+)
+
+func Test_login(t *testing.T) {
+	testData, err := testutil.ReadYamlFile("login.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, data := range testData {
+		t.Run(data.Name, func(t *testing.T) {
+			e, err := testutil.InitTest(t)
+			if err != nil {
+				t.Fatal(err)
+			}
+			responseBody := e.POST("/login").WithJSON(data.Data).Expect().Status(data.Expect["code"].(int)).JSON().Object()
+			responseBody.Value("code").Number().IsEqual(data.Expect["code"])
+			responseBody.Value("message").NotNull().IsString().NotEqual("")
+			responseData := responseBody.Value("data").Object()
+			responseData.Value("role").IsString().IsEqual(data.Expect["role"])
+			responseData.Value("username").IsString().IsEqual(data.Expect["username"])
+			responseData.Value("token").IsString().NotNull()
+		})
+	}
+}
