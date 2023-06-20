3a77480b78a2dab55eaf9bd0473a03d5997bafd0
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-06-20 22:53:00 +0700

    add assert to db audit for login test

--- end2end/login_test.go
+++ end2end/login_test.go
@@ -1,7 +1,9 @@
 package end2end
 
 import (
+	"github.com/stretchr/testify/assert"
 	"nashrul-be/crm/end2end/testutil"
+	"nashrul-be/crm/entities"
 	"testing"
 )
 
@@ -16,13 +18,24 @@ func Test_login(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			responseBody := e.POST("/login").WithJSON(data.Data).Expect().Status(data.Expect["code"].(int)).JSON().Object()
+			req := e.POST("/login").WithJSON(data.Data).Expect().Status(data.Expect["code"].(int))
+			req.Header("Set-Cookie").NotEmpty()
+			responseBody := req.JSON().Object()
 			responseBody.Value("code").Number().IsEqual(data.Expect["code"])
 			responseBody.Value("message").NotNull().IsString().NotEqual("")
 			responseData := responseBody.Value("data").Object()
 			responseData.Value("role").IsString().IsEqual(data.Expect["role"])
 			responseData.Value("username").IsString().IsEqual(data.Expect["username"])
-			responseData.Value("token").IsString().NotNull()
+			db, err := testutil.GetConn()
+			if err != nil {
+				t.Fatal(err)
+			}
+			var audit entities.Audit
+			if err := db.Order("date_time desc").First(&audit).Error; err != nil {
+				t.Fatal(err)
+			}
+			assert.Equal(t, data.Expect["username"], audit.Username)
+			assert.Equal(t, "Login", audit.Action)
 		})
 	}
 }
