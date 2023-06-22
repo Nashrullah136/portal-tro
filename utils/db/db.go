14d44f290aaa8d12200ed37327cce4f4bc641aaf
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-06-22 20:42:08 +0700

    Add connect to sql server

--- utils/db/db.go
+++ utils/db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
 	"os"
 	"time"
@@ -22,18 +23,42 @@ func DsnWithConfig(config Config) string {
 	return dsn
 }
 
-func DsnWithEnv() string {
+func DsnMySQL() string {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
 		os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
 	return dsn
 }
 
-func Connect(dsn string) (*gorm.DB, error) {
+func DsnSqlServer(prefix string) string {
+	prefix = prefix + "_"
+	dsn := fmt.Sprintf("sqlserver://%s:%s@%s:%s/%s",
+		os.Getenv(prefix+"DB_USERNAME"), os.Getenv(prefix+"DB_PASSWORD"),
+		os.Getenv(prefix+"DB_HOST"), os.Getenv(prefix+"DB_PORT"), os.Getenv(prefix+"DB_NAME"))
+	return dsn
+}
+
+func ConnectMySql(dsn string) (*gorm.DB, error) {
 	var dbConn *gorm.DB
 	var err error
 	for i := 0; i < 10; i++ {
 		dbConn, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			break
+		}
+		time.Sleep(2 * time.Second)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return dbConn, nil
+}
+
+func ConnectSqlServer(dsn string) (*gorm.DB, error) {
+	var dbConn *gorm.DB
+	var err error
+	for i := 0; i < 10; i++ {
+		dbConn, err = gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
 		if err == nil {
 			break
 		}
