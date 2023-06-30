73b6129bd68a84bf459931616596c21fc25ed51a
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-06-30 13:55:38 +0700

    refactor server-utilization.go to use zabbix cache

--- modules/worker/server-utilization.go
+++ modules/worker/server-utilization.go
@@ -1,37 +1,26 @@
 package worker
 
 import (
-	"context"
-	"encoding/json"
 	"github.com/go-co-op/gocron"
-	"github.com/redis/go-redis/v9"
 	"log"
 	"nashrul-be/crm/entities"
-	serverutilization "nashrul-be/crm/modules/server-utilization"
 	"nashrul-be/crm/utils/zabbix"
 	"time"
 )
 
-func UpdateLastDataServerUtil(client *redis.Client, api zabbix.API) error {
+func UpdateLastDataServerUtil(cache zabbix.Cache, api zabbix.API) error {
 	wib, _ := time.LoadLocation("Asia/Jakarta")
 	s := gocron.NewScheduler(wib)
 	_, err := s.Every(1).Minute().Do(func() {
-		templateJson := client.Get(context.Background(), serverutilization.ZabbixTemplate)
-		if templateJson.Err() != nil {
-			log.Fatal("Failed to access redis")
+		template, err := cache.GetTemplate()
+		if err != nil {
+			log.Fatal("can't get template for server util")
 		}
-		var template []entities.ServerUtilization
-		if err := json.Unmarshal([]byte(templateJson.String()), &template); err != nil {
-			log.Fatal("Malformed zabbix template")
+		itemIds, err := cache.GetItemIds()
+		if err != nil {
+			log.Fatal("can't get item ids for server util")
 		}
-		itemIdsJson := client.Get(context.Background(), serverutilization.ZabbixItemIds)
-		if itemIdsJson.Err() != nil {
-			log.Fatal("Failed to access redis")
-		}
-		var itemIds []string
-		if err := json.Unmarshal([]byte(itemIdsJson.String()), &itemIds); err != nil {
-			log.Fatal("Malformed zabbix item ids")
-		}
+		result := make([]entities.ServerUtilization, 18)
 		history, err := api.GetHistoryFromItem(itemIds)
 		if err != nil {
 			log.Fatal("Failed to access zabbix server")
@@ -40,7 +29,6 @@ func UpdateLastDataServerUtil(client *redis.Client, api zabbix.API) error {
 		for _, val := range history {
 			historyMap[val.ItemId] = val.Value
 		}
-		result := make([]entities.ServerUtilization, 18)
 		for _, serverUtil := range template {
 			temp := entities.ServerUtilization{
 				CpuPercentage: historyMap[serverUtil.CpuPercentage],
@@ -52,11 +40,7 @@ func UpdateLastDataServerUtil(client *redis.Client, api zabbix.API) error {
 			}
 			result = append(result, temp)
 		}
-		resultJson, err := json.Marshal(result)
-		if err != nil {
-			log.Fatal("Failed to marshal result")
-		}
-		if status := client.Set(context.Background(), serverutilization.ZabbixLastValue, resultJson, 0); status.Err() != nil {
+		if err := cache.SetLastValue(result); err != nil {
 			log.Fatal("Failed to save last value result to redis")
 		}
 	})
