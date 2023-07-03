5abd09f538ef1be5083ae244ad08bf2c43487894
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-07-03 13:25:13 +0700

    change date format to MM/DD/YYYY

--- modules/audit/dto.go
+++ modules/audit/dto.go
@@ -9,8 +9,8 @@ type GetAllRequest struct {
 	Username   string    `form:"username"`
 	Object     string    `form:"object"`
 	ObjectId   string    `form:"object_id"`
-	AfterDate  time.Time `form:"after_date"`
-	BeforeDate time.Time `form:"before_date"`
+	AfterDate  time.Time `form:"after_date" time_format:"01/02/2006"`
+	BeforeDate time.Time `form:"before_date" time_format:"01/02/2006"`
 	Page       int       `form:"page,omitempty"`
 	PerPage    int       `form:"perpage"`
 }
