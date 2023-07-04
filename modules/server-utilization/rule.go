af592f19c7340d134b75e3da8832f10aad028046
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-07-04 16:43:48 +0700

    update rule server utilization

--- modules/server-utilization/rule.go
+++ modules/server-utilization/rule.go
@@ -3,29 +3,28 @@ package server_utilization
 import (
 	"nashrul-be/crm/entities"
 	"strconv"
-	"time"
 )
 
 type ThresholdFunc func(utilization entities.ServerUtilization) bool
 
 func CheckCPU(utilization entities.ServerUtilization) bool {
-	cpu, _ := strconv.Atoi(utilization.CpuPercentage)
+	cpu, _ := strconv.ParseFloat(utilization.CpuPercentage, 32)
 	return cpu < 90
 }
 
 func CheckMemory(utilization entities.ServerUtilization) bool {
-	memory, _ := strconv.Atoi(utilization.MemoryUsage)
+	memory, _ := strconv.ParseFloat(utilization.MemoryUsage, 32)
 	return memory < 90
 }
 
 func CheckUptime(utilization entities.ServerUtilization) bool {
 	upTime, _ := strconv.Atoi(utilization.SystemUptime)
-	return upTime < int(30*24*time.Hour)
+	return upTime < 3*2628000
 }
 
 func CheckDisk(utilization entities.ServerUtilization) bool {
 	for _, disk := range utilization.Disks {
-		percent, _ := strconv.Atoi(disk)
+		percent, _ := strconv.ParseFloat(disk, 32)
 		if percent >= 95 {
 			return false
 		}
