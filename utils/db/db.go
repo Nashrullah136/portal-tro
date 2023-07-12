013a72ca2993e582c3b998e3cf794dc4ec7d82c5
Muhammad Nashrullah <muhamad.nasrul29@gmail.com>
2023-07-12 21:34:06 +0700

    refactor db utils and add connecting status

--- utils/db/db.go
+++ utils/db/db.go
@@ -5,18 +5,13 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
+	"log"
 	"nashrul-be/crm/utils/localtime"
 	"os"
 	"time"
 )
 
-type Config struct {
-	User     string
-	Password string
-	Host     string
-	Port     string
-	DBName   string
-}
+type openFunc func(string) gorm.Dialector
 
 func GormConfig() *gorm.Config {
 	return &gorm.Config{
@@ -27,23 +22,30 @@ func GormConfig() *gorm.Config {
 	}
 }
 
-func DsnWithConfig(config Config) string {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", config.User, config.Password,
-		config.Host, config.Port, config.DBName)
-	return dsn
+func Connect(prefix string) (connDb *gorm.DB, err error) {
+	log.Printf("Connecting to DB %s...\n", prefix)
+	prefixResult := prefixRule(prefix)
+	switch os.Getenv(prefixResult + "DB_DRIVER") {
+	case "mysql":
+		dsn := DsnMySQL(prefix)
+		connDb, err = ConnectMySql(dsn)
+	case "mssql":
+		dsn := DsnSqlServer(prefix)
+		connDb, err = ConnectSqlServer(dsn)
+	}
+	return
 }
 
-func DsnMySQL() string {
+func DsnMySQL(prefix string) string {
+	prefix = prefixRule(prefix)
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
-		os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+		os.Getenv(prefix+"DB_USERNAME"), os.Getenv(prefix+"DB_PASSWORD"),
+		os.Getenv(prefix+"DB_HOST"), os.Getenv(prefix+"DB_PORT"), os.Getenv(prefix+"DB_NAME"))
 	return dsn
 }
 
 func DsnSqlServer(prefix string) string {
-	if prefix != "" {
-		prefix = prefix + "_"
-	}
+	prefix = prefixRule(prefix)
 	dsn := fmt.Sprintf("sqlserver://%s:%s@%s:%s/%s",
 		os.Getenv(prefix+"DB_USERNAME"), os.Getenv(prefix+"DB_PASSWORD"),
 		os.Getenv(prefix+"DB_HOST"), os.Getenv(prefix+"DB_PORT"), os.Getenv(prefix+"DB_NAME"))
@@ -51,26 +53,23 @@ func DsnSqlServer(prefix string) string {
 }
 
 func ConnectMySql(dsn string) (*gorm.DB, error) {
-	var dbConn *gorm.DB
-	var err error
-	for i := 0; i < 10; i++ {
-		dbConn, err = gorm.Open(mysql.Open(dsn), GormConfig())
-		if err == nil {
-			break
-		}
-		time.Sleep(2 * time.Second)
-	}
-	if err != nil {
-		return nil, err
-	}
-	return dbConn, nil
+	return connectDB(mysql.Open, dsn)
 }
 
 func ConnectSqlServer(dsn string) (*gorm.DB, error) {
-	var dbConn *gorm.DB
-	var err error
+	return connectDB(sqlserver.Open, dsn)
+}
+
+func prefixRule(prefix string) string {
+	if prefix != "" {
+		prefix = prefix + "_"
+	}
+	return prefix
+}
+
+func connectDB(open openFunc, dsn string) (dbConn *gorm.DB, err error) {
 	for i := 0; i < 10; i++ {
-		dbConn, err = gorm.Open(sqlserver.Open(dsn), GormConfig())
+		dbConn, err = gorm.Open(open(dsn), GormConfig())
 		if err == nil {
 			break
 		}
